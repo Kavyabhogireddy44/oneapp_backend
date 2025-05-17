d32849a837555280ccae4a29dece6d12289362d1
Kavya <kavyabhogireddy@gmail.com>
2025-05-17 15:30:15 +0530

    added check user api

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
+from .views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView, CheckPhoneNumberAPIView
 
 urlpatterns = [
     path('', UserListCreateAPIView.as_view(), name='user-list-create'),
     path('<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view(), name='user-detail'),
+    path('check-phone/', CheckPhoneNumberAPIView.as_view(), name='check-phone'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 class UserListCreateAPIView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
@@ -10,3 +13,12 @@ class UserListCreateAPIView(generics.ListCreateAPIView):
 class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+class CheckPhoneNumberAPIView(APIView):
+    def post(self, request):
+        phone = request.data.get('phone')
+        if not phone:
+            return Response(False, status=status.HTTP_200_OK)  # or return an error if preferred
+        
+        exists = CustomUser.objects.filter(phone=phone).exists()
+        return Response(exists, status=status.HTTP_200_OK)
