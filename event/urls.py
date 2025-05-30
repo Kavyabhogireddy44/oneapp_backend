fdd2c1be0b4a55062e1ec6a70242d66836a001c5
Kavya <kavyabhogireddy@gmail.com>
2025-05-30 23:50:59 +0530

    Event changes again  added

--- event/urls.py
+++ event/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<int:pk>/', EventRetrieveUpdateAPIView.as_view(), name='event-detail'),
     path('event-by-token/', EventsByTokenAPIView.as_view(), name='events-by-token'),
     path('events-by-token/',EventsListByTokenAPIView.as_view(), name='events-list-by-token'),
-    path('delete-events-by-token/',DeleteEventsByTokenAPIView.as_view(), name='delete-events-by-token'),
+    path('delete-events-by-token/<int:pk>/',DeleteEventsByTokenAPIView.as_view(), name='delete-events-by-token'),
 ]
