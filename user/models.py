20302ed868cdf68818373a8316b4b59a47985def
Kavya <kavyabhogireddy@gmail.com>
2025-08-15 16:23:51 +0530

    fcm column added

--- user/models.py
+++ user/models.py
@@ -5,11 +5,13 @@ class CustomUser(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
+    fcm_token = models.CharField(max_length=255, blank=True)
     profile_image = models.URLField(blank=True)
     phone = models.CharField(max_length=20, blank=True)
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+    last_login= models.DateTimeField(auto_now=True)
     role = models.CharField(max_length=50, choices=[
         ('user', 'User'),
         ('provider', 'Service Provider'),
